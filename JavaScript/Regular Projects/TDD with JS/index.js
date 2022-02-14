74a5afdec2b16a9fc4865022080f6b9da05983a3
Mac M <daniel.m.mckee90@gmail.com>
2022-02-14 18:47:01 -0500

    TDD Back to green - fix edge case

--- JavaScript/Regular Projects/TDD with JS/index.js
+++ JavaScript/Regular Projects/TDD with JS/index.js
@@ -1,12 +1,13 @@
 const Calculate = {
   factorial(inputNumber) {
-      for (let i = inputNumber -1; i>= 1; i--) {
-          inputNumber *= i
-      }
-      return inputNumber;
-  }
-}
+    if (inputNumber === 0) {
+      return 1;
+    }
+    for (let i = inputNumber - 1; i >= 1; i--) {
+      inputNumber *= i;
+    }
+    return inputNumber;
+  },
+};
 
 module.exports = Calculate;
-
-
