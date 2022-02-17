0277a693d6339ce01f849b1f8793bbbac2961d4b
Mac M <daniel.m.mckee90@gmail.com>
2022-02-17 01:01:56 -0500

    moving reactapp into react folder

--- React/myfirstreactapp/src/App.js
+++ React/myfirstreactapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Michael, be better at programming.
         </p>
         <a
           className="App-link"
