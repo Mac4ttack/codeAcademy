a6c4bd042fc6dc59828091e8e1dcdb75262a2e35
Mac M <daniel.m.mckee90@gmail.com>
2022-02-28 14:46:14 -0500

    Update to Examples and Social Network proj

---
+++ React/Pet Social Network/readme.md
@@ -0,0 +1,3 @@
+This project focused on mounting, unmounting, updating, and making sure to render correctly as those occurred.
+
+Did not include the entire file structure from project.
--- React/Video Player/Menu.js
+++ React/Video Player/Menu.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Video } from './Video';
+import { Menu } from './Menu';
 
-export class Menu extends React.Component {
+const VIDEOS = {
+  fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+  slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+  cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+  eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { src: VIDEOS.fast };
+    this.chooseVideo = this.chooseVideo.bind(this);
+  }
+ chooseVideo(newVideo) {
+   this.setState({
+     src: VIDEOS[newVideo]
+   })
+ } 
+
   render() {
     return (
-      <form>
-        <input type="radio" name="src" value="fast" /> fast
-        <input type="radio" name="src" value="slow" /> slow
-        <input type="radio" name="src" value="cute" /> cute
-        <input type="radio" name="src" value="eek" /> eek
-      </form>
+      <div>
+        <h1>Video Player</h1>
+        <Menu chooseVideo={this.chooseVideo}/>
+        <Video src={this.state.src} />
+      </div>
     );
   }
 }
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('app')
+);
