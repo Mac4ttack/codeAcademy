6f5c29a91ac0c202ada4ff60354821844aeecc1d
Mac M <daniel.m.mckee90@gmail.com>
2022-03-03 11:34:50 -0500

    finished state/effect hooks init  passing thoughts

--- React/Example Code/Hooks/Effect Hooks/examples.js
+++ React/Example Code/Hooks/Effect Hooks/examples.js
@@ -55,3 +55,185 @@ export default function Timer() {
     </>
   );
 }
+
+
+
+/// only use hooks at top level
+
+import React, { useState, useEffect } from 'react';
+import { get } from './mockBackend/fetch';
+
+export default function Shop() {
+  const [categories, setCategories] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [items, setItems] = useState({});
+
+  useEffect(() => {
+    get('/categories').then((response) => {
+      setCategories(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (selectedCategory && !items[selectedCategory]) {
+      get(`/items?category=${selectedCategory}`).then((response) => {
+        setItems((prev) => ({ ...prev, [selectedCategory]: response.data }));
+      });
+    }
+  }, [items, selectedCategory]);
+
+  if (!categories) {
+    return <p>Loading..</p>;
+  }
+
+  return (
+    <div className='App'>
+      <h1>Clothes 'n Things</h1>
+      <nav>
+        {categories.map((category) => (
+          <button key={category} onClick={() => setSelectedCategory(category)}>
+            {category}
+          </button>
+        ))}
+      </nav>
+      <h2>{selectedCategory}</h2>
+      <ul>
+        {!items[selectedCategory]
+          ? null
+          : items[selectedCategory].map((item) => <li key={item}>{item}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+
+
+//separate hooks for separate effects
+
+//bad
+
+// Handle both position and menuItems with one useEffect hook.
+const [data, setData] = useState({ position: { x: 0, y: 0 } });
+useEffect(() => {
+  get('/menu').then((response) => {
+    setData((prev) => ({ ...prev, menuItems: response.data }));
+  });
+  const handleMove = (event) =>
+    setData((prev) => ({
+      ...prev,
+      position: { x: event.clientX, y: event.clientY }
+    }));
+  window.addEventListener('mousemove', handleMove);
+  return () => window.removeEventListener('mousemove', handleMove);
+}, []);
+
+//good
+
+// Handle menuItems with one useEffect hook.
+const [menuItems, setMenuItems] = useState(null);
+useEffect(() => {
+  get('/menu').then((response) => setMenuItems(response.data));
+}, []);
+ 
+// Handle position with a separate useEffect hook.
+const [position, setPosition] = useState({ x: 0, y: 0 });
+useEffect(() => {
+  const handleMove = (event) =>
+    setPosition({ x: event.clientX, y: event.clientY });
+  window.addEventListener('mousemove', handleMove);
+  return () => window.removeEventListener('mousemove', handleMove);
+}, []);
+
+
+//// separate calls --- 3 state and effect hooks instead of one big one called "menu"
+//refactored JSX to accept new structure
+
+import React, { useState, useEffect } from 'react';
+import { get } from './mockBackend/fetch';
+
+export default function SocialNetwork() {
+  const [menu, setMenu] = useState(null);
+  useEffect(() => {
+    get('/menu').then((response) => {
+      setMenu(response.data);
+    });
+  }, []);
+
+  const [newsFeed, setNewsFeed] = useState(null);
+  useEffect(() => {
+    get('/news-feed').then((response) => {
+      setNewsFeed(response.data);
+    });
+  }, []);
+
+  const [friends, setFriends] = useState(null);
+  useEffect(() => {
+    get('/friends').then((response) => {
+      setFriends(response.data);
+    });
+  }, []);
+
+  return (
+    <div className='App'>
+      <h1>My Network</h1>
+      {!menu ? (
+        <p>Loading..</p>
+      ) : (
+        <nav>
+          {menu.map((menuItem) => (
+            <button key={menuItem}>{menuItem}</button>
+          ))}
+        </nav>
+      )}
+      <div className='content'>
+        {!newsFeed ? (
+          <p>Loading..</p>
+        ) : (
+          <section>
+            {newsFeed.map(({ id, title, message, imgSrc }) => (
+              <article key={id}>
+                <h3>{title}</h3>
+                <p>{message}</p>
+                <img src={imgSrc} alt='' />
+              </article>
+            ))}
+          </section>
+        )}
+        {!friends ? (
+          <p>Loading..</p>
+        ) : (
+          <aside>
+            <ul>
+              {friends
+                .sort((a, b) => (a.isOnline && !b.isOnline ? -1 : 0))
+                .map(({ id, name, isOnline }) => (
+                  <li key={id} className={isOnline ? 'online' : 'offline'}>
+                    {name}
+                  </li>
+                ))}
+            </ul>
+          </aside>
+        )}
+      </div>
+    </div>
+  );
+}
+
+
+/*main concepts
+Let’s review the main concepts from this lesson:
+
+useEffect() - we can import this function from the 'react' library and call it in our function components
+effect - refers to a function that we pass as the first argument of the useEffect() function. 
+By default, the Effect Hook calls this effect after each render
+
+cleanup function - the function that is optionally returned by the effect. 
+If the effect does anything that needs to be cleaned up to prevent memory leaks, then the effect returns a cleanup function, 
+then the Effect Hook will call this cleanup function before calling the effect again as well as when the component is being 
+unmounted
+
+dependency array - this is the optional second argument that the useEffect() function can be called with in order 
+to prevent repeatedly calling the effect when this is not needed. This array should consist of all variables that 
+the effect depends on.
+
+*/
---
+++ React/Passing Thoughts/readme.md
@@ -0,0 +1,7 @@
+Passing Thoughts
+
+What if you could post something and know it wouldn’t live forever? In this project, we’ll build a place for our passing thoughts. Once you add a short thought, it’ll disappear after just 15 seconds.
+
+Let’s get started!
+
+Guided instructions and starting resources provided
