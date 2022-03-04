fecc8aacd2df339668d738ff39a56b8252de588d
Mac M <daniel.m.mckee90@gmail.com>
2022-03-04 11:36:24 -0500

    Initial functionality added - updating adopt project

--- React/adopt-a-pet-starter/src/App.js
+++ React/adopt-a-pet-starter/src/App.js
@@ -3,13 +3,19 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
+import { BrowserRouter as Router } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Route>
+          <HomePage path='HomePage/:type?' />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
