6a131ff766c42e7423f9a5bc66e9555276a181cc
Mac M <daniel.m.mckee90@gmail.com>
2022-03-23 22:14:43 -0400

    Extra Challenge store (searchTerm) completed

--- Redux/codecademystore/src/app/store.js
+++ Redux/codecademystore/src/app/store.js
@@ -4,10 +4,12 @@ import { createStore, combineReducers} from 'redux';
 import {cartReducer} from '../features/cart/cartSlice'
 import {inventoryReducer} from '../features/inventory/inventorySlice'
 import {currencyFilterReducer} from '../features/currencyFilter/currencyFilterSlice'
+import {searchTermReducer} from '../features/searchTerm/searchTermSlice'
 
 // Create and export the store here.
 export const store = createStore(combineReducers({
   cart: cartReducer,
   inventory: inventoryReducer,
-  currencyFilter: currencyFilterReducer
+  currencyFilter: currencyFilterReducer,
+  searchTerm: searchTermReducer
 }));
