35686acf3389e88e7b000eb5a230e0c96126c5d7
Mac M <daniel.m.mckee90@gmail.com>
2022-05-01 16:51:19 -0400

    Completed  Film Finder

--- JavaScript/Challenge Projects/Film Finder/public/script.js
+++ JavaScript/Challenge Projects/Film Finder/public/script.js
@@ -1,43 +1,78 @@
-require('dotenv').config()
+//const dotenv = require('../dotenv').config();
+//const tmdbKey = process.env.Movie_db_api;
 
-const tmdbKey = process.env.Movie_db_api;
-const tmdbBaseUrl = 'https://api.themoviedb.org/3';
-const playBtn = document.getElementById('playBtn');
-
+const tmdbKey = "";
+const tmdbBaseUrl = "https://api.themoviedb.org/3";
+const playBtn = document.getElementById("playBtn");
 
 const getGenres = async () => {
-    const genreRequestEndpoint = '/genre/movie/list';
-    const requestParams = `?api_key=${tmdbKey}`;
-    const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`
+  const genreRequestEndpoint = "/genre/movie/list";
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
 
-  try{
-      const response = await fetch(urlToFetch);
-      if (response.ok) {
-        const jsonResponse = await response.json();
-        return jsonResponse.genres;
-      }
-      throw new Error('Request failed!');
-    } catch (error) {
-      console.log(error);	
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse.genres;
     }
-  };
+    throw new Error("Request failed!");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const getMovies = () => {
-  const selectedGenre = getSelectedGenre();
+const getMovies = async () => {
+    const selectedGenre = getSelectedGenre();
+    const discoverMovieEndpoint = '/discover/movie';
+    const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+    const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+
+    console.log(urlToFetch);
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+          const jsonResponse = await response.json();
+          const movies = jsonResponse.results;
+          return movies;
+        }
+        throw new Error("Request failed!");
+      } catch (error) {
+        console.log(error);
+      }
 
 };
 
-const getMovieInfo = () => {
+const getMovieInfo = async (movie) => {
+    const movieId = movie.id;
+    const movieEndpoint = `/movie/${movieId}`;
+    const requestParams = `?api_key=${tmdbKey}`;
+    const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
 
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+          const jsonResponse = await response.json();
+          const movieInfo = jsonResponse;
+          return movieInfo;
+        }
+        throw new Error("Request failed!");
+      } catch (error) {
+        console.log(error);
+      }
 };
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
-const showRandomMovie = () => {
-  const movieInfo = document.getElementById('movieInfo');
+const showRandomMovie = async () => {
+  const movieInfo = document.getElementById("movieInfo");
   if (movieInfo.childNodes.length > 0) {
     clearCurrentMovie();
-  };
+  }
 
+  const movies = await getMovies();
+  const randomMovie = getRandomMovie(movies);
+  const info = await getMovieInfo(randomMovie);
+  displayMovie(info);
 };
 
 getGenres().then(populateGenreDropdown);
